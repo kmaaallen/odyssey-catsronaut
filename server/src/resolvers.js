1e771ac879434b70e645b4501ae7e5e4767572a8
Kirsten Allen <kirsten.allen@lego.com>
2022-10-28 14:24:50 +0100

    feat: Add increment views mutation

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -10,6 +10,27 @@ const resolvers = {
       return dataSources.trackAPI.getTrack(id);
     },
   },
+  Mutation: {
+    // returns track with incremented view
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track: track,
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null,
+        };
+      }
+    },
+  },
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
